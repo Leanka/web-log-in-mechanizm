809e03e319f9166d0cadf5392b06356343e5f4fb
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-04-23 20:12:34 +0200

    Create random id generator in place od current id placeholder

--- src/main/java/service/IdProvider.java
+++ src/main/java/service/IdProvider.java
@@ -1,10 +1,47 @@
 package service;
 
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class IdProvider {
-    private static int counter = 0;
+    private static SecureRandom random = new SecureRandom();
+    private static final String ALPHA = "abcdefghijklmnopqrstuvwxyz";
+    private static final String NUMERIC = "0123456789";
+    private static final String SPECIAL_CHARS = "!@#$%^&*_+-/";
 
     public static String getId(){
-        return "placeholderCounterNr" + counter++;
+        return generateId();
+    }
+
+    private static String generateId(){
+        List<Character> characters = pickRandomCharacters();
+        return characters.stream()
+                        .map(character -> character.toString())
+                        .collect(Collectors.joining());
+    }
+
+    private static Character getRandomCharacter(String tokens){
+        int charIndex = random.nextInt(tokens.length());
+        return tokens.charAt(charIndex);
+    }
+
+    private static List<Character> pickRandomCharacters(){
+        List<Character> characters = new ArrayList<>();
+
+        int loop = 2;
+        do{
+            characters.add(getRandomCharacter(ALPHA));
+            characters.add(getRandomCharacter(ALPHA.toUpperCase()));
+            characters.add(getRandomCharacter(NUMERIC));
+            characters.add(getRandomCharacter(SPECIAL_CHARS));
+        }while (--loop > 0);
+
+        Collections.shuffle(characters);
+
+        return characters;
     }
 
 }
